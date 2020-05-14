70b07ef2554403cb9e70763ab6e91adaa2d1e2e3
hoanghihi6699 <trananhhoang6699@gmail.com>
2020-05-14 09:33:29 +0700

    Commit 1

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+// Check cookie
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function checkCookie() {
+    var username = getCookie("username");
+    if (username != "") {
+        console.log(username);
+    } else {
+        username = "hoang";
+        if (username != "" && username != null) {
+            setCookie("username", username, 365);
+            document.getElementById('showdemo').click();
+        }
+    }
+}
+function removeCookie() {
+    console.log(getCookie("username"));
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+}
+
